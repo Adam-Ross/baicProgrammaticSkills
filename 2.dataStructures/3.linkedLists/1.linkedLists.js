58caebcf72889cbb0e22bcec7af3416191fb1c3e
Adam Ross <adamgarrettross@gmail.com>
2020-07-21 14:42:23 -0500

    Delete

--- 2.dataStructures/3.linkedLists/1.linkedLists.js
+++ 2.dataStructures/3.linkedLists/1.linkedLists.js
@@ -57,47 +57,6 @@ class LinkedList {
     return this;
   }
 
-  deleteHead() {
-    this.head = this.head.next;
-    this.length--;
-  }
-
-  deleteTail() {
-    let counter = 0;
-    let current = this.head;
-    while (counter < this.length - 1) {
-      current = current.next;
-      counter++;
-    }
-    current.next = null;
-    this.tail = current;
-    console.log(current);
-    this.length--;
-  }
-
-  deleteByIndex(index) {
-    if (index >= this.length || index < 0) {
-      return -1;
-    } else if (index === 0) {
-      this.deleteHead();
-    } else if (index === this.length - 1) {
-      this.deleteTail();
-    } else {
-      let current = this.head;
-      let count = 0;
-      while (count < index - 1) {
-        current = current.next;
-        count++;
-      }
-      current.next = current.next.next;
-      this.length--;
-    }
-  }
-
-  // deleteByValue() {
-
-  // }
-
   insert(index, data) {
     if (index < 0 || index > this.length) {
       return -1;
@@ -121,6 +80,43 @@ class LinkedList {
     }
     return this;
   }
+
+  removeHead() {
+    this.head = this.head.next;
+    this.length--;
+  }
+
+  removeTail() {
+    let current = this.head;
+    let count = 0;
+
+    while (count < this.length - 2) {
+      current = current.next;
+      count++;
+    }
+    current.next = null;
+    this.tail = current;
+    this.length--;
+  }
+
+  delete(index) {
+    if (index < 0 || index >= this.length) {
+      return -1;
+    } else if (index === 0) {
+      this.removeHead();
+    } else if (index === this.length - 1) {
+      this.removeTail();
+    } else {
+      let current = this.head;
+      let count = 0;
+      while (count < index - 1) {
+        current = current.next;
+        count++;
+      }
+      current.next = current.next.next;
+      this.length--;
+    }
+  }
 }
 
 const list = new LinkedList();
@@ -132,9 +128,9 @@ list.append(list.length);
 list.append(list.length);
 list.append(list.length);
 list.append(list.length);
-list.insert(5, "This will be inserted at index 5");
-console.log(list.getProps());
+list.insert(5, "This is going to be at index 5");
 console.log(list.print());
-list.deleteByIndex(5);
+console.log(list.getProps());
+list.delete(5);
 console.log(list.print());
 console.log(list.getProps());
