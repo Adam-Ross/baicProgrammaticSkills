e570d0780e24acdee3b4c3dfbc5c6de8955388e2
Adam Ross <adamgarrettross@gmail.com>
2020-08-07 12:15:07 -0500

    stacks and other thing

--- 2.dataStructures/6.trees/trees.js
+++ 2.dataStructures/6.trees/trees.js
@@ -1,4 +1,46 @@
-// Code out BST
+// // Code out BST
+// class Node {
+//   constructor(data) {
+//     this.data = data;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+// class BST {
+//   constructor() {
+//     this.root = null;
+//   }
+
+//   insert(data) {
+//     const node = new Node(data.id);
+//     if (!this.root) {
+//       this.root = node;
+//     } else {
+//       let current = this.root;
+//       while (true) {
+//         if (data < current.data) {
+//           // Move left
+//           if (!current.left) {
+//             current.left = node;
+//             return this;
+//           } else {
+//             current = current.left;
+//           }
+//         } else {
+//           // Move right
+//           if (!current.right) {
+//             current.right = node;
+//             return this;
+//           } else {
+//             current = current.right;
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+
 class Node {
   constructor(data) {
     this.data = data;
@@ -19,8 +61,7 @@ class BST {
     } else {
       let current = this.root;
       while (true) {
-        if (data < current.data) {
-          // Move left
+        if (data.id < current.data.id) {
           if (!current.left) {
             current.left = node;
             return this;
@@ -28,7 +69,6 @@ class BST {
             current = current.left;
           }
         } else {
-          // Move right
           if (!current.right) {
             current.right = node;
             return this;
@@ -39,12 +79,59 @@ class BST {
       }
     }
   }
+
+  lookup(id) {
+    if (!this.root) {
+      return -1;
+    }
+
+    if (this.root.data.id === id) {
+      return this.root.data;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (id === current.data.id) {
+          return current.data;
+        }
+
+        if (id < current.data.id) {
+          // Move left
+          if (!current.left) {
+            return "Not found";
+          } else {
+            current = current.left;
+          }
+        } else {
+          if (!current.right) {
+            return "Not found";
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
 }
 
+const data = {
+  name: "Mike",
+  age: "24",
+  id: 20,
+};
+
+const data2 = {
+  name: "Mike",
+  age: "34",
+  id: 10,
+};
+
+const data3 = {
+  name: "Mike",
+  age: "34",
+  id: 5,
+};
 const bst = new BST();
-bst.insert(20);
-bst.insert(30);
-bst.insert(10);
-bst.insert(5);
-bst.insert(40);
-console.log(bst);
+bst.insert(data);
+bst.insert(data2);
+bst.insert(data3);
+console.log(bst.lookup(20));
